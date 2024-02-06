411103665d48d2664614b2593011ba23953016c3
Mayank Deep <mayankdeep217@gmail.com>
2024-02-07 02:37:48 +0530

    adding initial event listeners

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log("JS is Connected...");
+
+let getTodobtn = document.getElementById("get_todos");
+
+getTodobtn.addEventListener("click" , () => {
+    console.log("Clicked the item");
+});
+
+        // class access krne ke liye index based accessing krna pdega 
+
+// let taskNumber = document.getElementsByClassName("task_number")[1];
+
+// taskNumber.addEventListener("click" , () => {
+//     console.log("Clicked the item");
+// });
+
+            // or like this 
+// let taskNumber = document.getElementsByClassName("task_number");
+
+// taskNumber[1].addEventListener("click" , () => {
+//     console.log("Clicked the item");
+// });
