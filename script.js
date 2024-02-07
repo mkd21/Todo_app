eaea22188aa7cf089dbced8fbd1b2db9502ee7a3
Mayank Deep <mayankdeep217@gmail.com>
2024-02-08 02:38:31 +0530

    made add todo functionality

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
 // console.log("JS is Connected...");
 
-let getTodobtn = document.getElementById("get_todos");
+let parentoftasks = document.getElementById("taskParent");
 
-getTodobtn.addEventListener("click" , () => {
-    console.log("Clicked the item");
-});
+function addTodo(todo_data)
+{
+    let rowDiv = document.createElement("div");
+    let task_control_area = document.createElement("div");
 
-        // class access krne ke liye index based accessing krna pdega 
+    let task_number = document.createElement("div");
+    let actual_task = document.createElement("div");
+    let status_task = document.createElement("div");
 
-// let taskNumber = document.getElementsByClassName("task_number")[1];
+    let action_task = document.createElement("div");
+    let deleteBtn = document.createElement("button");
+    let finishedBtn = document.createElement("button");
 
-// taskNumber.addEventListener("click" , () => {
-//     console.log("Clicked the item");
-// });
+    let hr = document.createElement("hr");
 
-            // or like this 
-// let taskNumber = document.getElementsByClassName("task_number");
 
-// taskNumber[1].addEventListener("click" , () => {
-//     console.log("Clicked the item");
-// });
+
+            // ADDING CLASSES 
+
+    rowDiv.classList.add("row");
+    task_control_area.classList.add("row" ,"task_control_area", "d-flex" , "align-items-center" , "justify-content-center");
+    task_number.classList.add("task_number");
+    actual_task.classList.add("actual_task");
+    status_task.classList.add("status_task");
+    action_task.classList.add("action_task" ,"d-flex" , "justify-content-start");
+    deleteBtn.classList.add("btn" , "btn-danger");
+    finishedBtn.classList.add("btn" , "btn-success");
+        
+
+        // ADDING THE ACTUAL CONTENT 
+
+    task_number.textContent = "1";
+    actual_task.textContent = todo_data;     // sets the to do text after putting information 
+    status_task.textContent = "In Progress";
+    deleteBtn.textContent = "DELETE";
+    finishedBtn.textContent = "FINISHED";
+
+    action_task.appendChild(deleteBtn);
+    action_task.appendChild(finishedBtn);
+
+
+    task_control_area.appendChild(task_number);
+    task_control_area.appendChild(actual_task);
+    task_control_area.appendChild(status_task);
+    task_control_area.appendChild(action_task);
+
+    rowDiv.appendChild(task_control_area);
+    rowDiv.appendChild(hr);
+
+    parentoftasks.appendChild(rowDiv);
+}
