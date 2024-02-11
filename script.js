972a92047879c9df8128702378bb4cd8093ea1f9
Mayank Deep <mayankdeep217@gmail.com>
2024-02-12 02:05:57 +0530

    added delete functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // console.log("JS is Connected...");
 
-let parentoftasks = document.getElementById("taskParent");
+let todo_data_new = document.getElementById("todo_data_new");
 
 let saveButton = document.getElementById("save_btn");
 let input_bar = document.getElementById("input_bar");
 
-let count = [];
+let countArr = [];
 
 input_bar.addEventListener("keyup" , function toggleSaveBtn(){
 
@@ -32,13 +32,31 @@ saveButton.addEventListener("click" , function exec(){
         return;
     }
 
-    count.push(input_Data);     // pushing in array to maintain the length of the index
-    addTodo(input_Data , count);
+    countArr.push(input_Data);             // pushing in array to maintain the length of the index
+    addTodo(input_Data , countArr.length);
 
     input_bar.value = "";
 
     saveButton.classList.add("disabled");
 });
+
+
+function deleteTodo(event)
+{
+    // console.log(event.target.parentElement.parentElement.parentElement);
+    // event.target.parentElement.parentElement.parentElement.remove();
+
+    let btnTodelete = event.target;
+
+    let specificBtnIndex = Number(btnTodelete.getAttribute("todo_index"));
+
+    countArr.splice(specificBtnIndex - 1 , 1);
+
+    todo_data_new.innerHTML = "";
+    countArr.forEach( function iterator(element , index){
+        addTodo(element , index + 1);
+    }); 
+}
 
 function addTodo(todo_data , count)
 {
@@ -65,13 +83,16 @@ function addTodo(todo_data , count)
     actual_task.classList.add("actual_task");
     status_task.classList.add("status_task");
     action_task.classList.add("action_task" ,"d-flex" , "justify-content-start");
-    deleteBtn.classList.add("btn" , "btn-danger");
+    deleteBtn.classList.add("btn" , "btn-danger" , "deleteTodoBtn");
     finishedBtn.classList.add("btn" , "btn-success");
         
 
+    deleteBtn.setAttribute("todo_index" , count);
+    deleteBtn.onclick = deleteTodo;
+
         // ADDING THE ACTUAL CONTENT 
 
-    task_number.textContent = `${count.length}`;  // maintains the serial number
+    task_number.textContent = count; // maintains the serial number
     actual_task.textContent = todo_data;            // sets the to do text after putting information 
     status_task.textContent = "In Progress";
     deleteBtn.textContent = "DELETE";
@@ -89,5 +110,6 @@ function addTodo(todo_data , count)
     rowDiv.appendChild(task_control_area);
     rowDiv.appendChild(hr);
 
-    parentoftasks.appendChild(rowDiv);
+    todo_data_new.appendChild(rowDiv);
 }
+
