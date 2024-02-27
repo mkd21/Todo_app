be1f98d506c4baafecc2e982c8a1da4c461cd821
Mayank Deep <mayankdeep217@gmail.com>
2024-02-27 22:07:17 +0530

    completed todos app

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let todo_div = document.getElementById("todo_div");
 let saveButton = document.getElementById("save_btn");
 let input_bar = document.getElementById("input_bar");
 
+let getPendingTodo = document.getElementById("get_todos");
+
+
 let countArr = [];
 
 input_bar.addEventListener("keyup" , function toggleSaveBtn(){
@@ -42,6 +45,15 @@ saveButton.addEventListener("click" , function exec(){
     saveButton.classList.add("disabled");
 });
 
+function getPendingTodos(event)
+{
+    countArr = countArr.filter( (todos) => todos.status != "Finished");
+
+    todo_div.innerHTML = "";
+    countArr.forEach( function iterator(element , index){
+        addTodo(element , index +  1);
+    });
+}
 
 function deleteTodo(event)
 {
@@ -78,14 +90,60 @@ function finishedTodo(event)
         countArr[exactBtnNumber - 1].btnText = "Undo";
     }
 
+    // sorting the todos according to their status 
+    countArr.sort( (a , b) => {
+        if(a.status == "Finished")
+        {
+            return 1;
+        }
+        else 
+        {
+            return -1;
+        }
+    });
+
+
     // re-render the html 
     todo_div.innerHTML = "";
-
     countArr.forEach( function (element , value) {
         addTodo(element , value + 1);
     })
-    // console.log("here..");
 }
+
+function editTodo(event)
+{
+    let edit_btn = event.target;
+    let indexForAll = Number(edit_btn.getAttribute("todo_index"));
+
+    let targetDiv = document.querySelector( `div[todo_index = "${indexForAll}"]` );
+    let targetInput = document.querySelector( `input[todo_index = "${indexForAll}"]` );
+
+    targetDiv.style.display = "none";
+    targetInput.type = "text";
+
+
+    // giving a pre text inside the input field 
+    targetInput.value = targetDiv.textContent;
+}
+
+function afterEditingTodo(event)
+{
+    let targetInput = event.target;     // input tag mil jyega yaha pr
+
+    let index = Number(targetInput.getAttribute("todo_index"));
+
+    let divTarget = document.querySelector(`div[todo_index = "${index}" ]`);
+
+    if(event.key == "Enter")
+    {
+        divTarget.textContent = targetInput.value;
+        divTarget.style.display = "block";
+
+        targetInput.value = "";
+        targetInput.type = "hidden";
+    }
+}
+
 
 function addTodo(todoElements , count)
 {
@@ -99,6 +157,8 @@ function addTodo(todoElements , count)
     let action_task = document.createElement("div");
     let deleteBtn = document.createElement("button");
     let finishedBtn = document.createElement("button");
+    let editBtn = document.createElement("button");
+    let hiddenInput = document.createElement("input");
 
     let hr = document.createElement("hr");         // horizontal rule ie line seperating each todo
 
@@ -114,17 +174,34 @@ function addTodo(todoElements , count)
     action_task.classList.add("action_task" ,"d-flex" , "justify-content-start");
     deleteBtn.classList.add("btn" , "btn-danger" , "deleteTodoBtn");
     finishedBtn.classList.add("btn" , "btn-success");
-        
+    editBtn.classList.add("btn" , "btn-warning" , "editBtn");
+
+    hiddenInput.classList.add("form-control" , "actual_task");
+
 
     // SETTING ATTRIBUTES TO BUTTON 
-
     deleteBtn.setAttribute("todo_index" , count);
     deleteBtn.onclick = deleteTodo;
     
+
+    // get pending task button
+
+    getPendingTodo.onclick = getPendingTodos;
+
     // todo status change ke liye 
-    finishedBtn.setAttribute("todo_index" , count);
     finishedBtn.onclick = finishedTodo;
+    finishedBtn.setAttribute("todo_index" , count);
     
+    // edit button 
+    editBtn.setAttribute("todo_index" , count);
+    editBtn.onclick = editTodo;
+
+    // input tag
+    hiddenInput.setAttribute("todo_index" , count);
+    actual_task.setAttribute("todo_index" , count);
+
+    hiddenInput.addEventListener("keypress" , afterEditingTodo);
+
         // ADDING THE ACTUAL CONTENT 
 
     task_number.textContent = count;         // maintains the serial number
@@ -132,13 +209,17 @@ function addTodo(todoElements , count)
     status_task.textContent = todoElements.status;
     deleteBtn.textContent = "DELETE";
     finishedBtn.textContent = todoElements.btnText;
+    editBtn.textContent = "Edit";
 
     action_task.appendChild(deleteBtn);
     action_task.appendChild(finishedBtn);
-
+    action_task.appendChild(editBtn);
 
     task_control_area.appendChild(task_number);
     task_control_area.appendChild(actual_task);
+    task_control_area.appendChild(hiddenInput);
+    hiddenInput.setAttribute("type" , "hidden");
+
     task_control_area.appendChild(status_task);
     task_control_area.appendChild(action_task);
 
@@ -147,4 +228,3 @@ function addTodo(todoElements , count)
 
     todo_div.appendChild(rowDiv);
 }
-
